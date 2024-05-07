a475720888b9910e99dad2b5239256b870736b23
Marius van Zundert <marius.vanzundert@cerberos.dev>
2024-05-07 08:58:46 +0200

    Updated new layouts without screenshots

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-Voyager layout
+# Building your layout from source
 
-## Layer 0: Main
+Congratulations on taking the next step, and making use of your keyboard's open-source nature! There's so much power to unlock — this is going to be fun. :)
 
-![Layer 0 Main ](https://github.com/Mvzundert/voyager-layout/assets/6066795/6de40584-5910-4439-88ad-32d027405e8c)
+Here are some initial pointers to get you started:
 
-## Layer 1: Sym + Num
+- Use the documentation at [https://docs.qmk.fm/](https://docs.qmk.fm/) to set up your environment for building your firmware.
+- Build your layout against [https://github.com/zsa/qmk_firmware/](https://github.com/zsa/qmk_firmware/) which is our QMK fork (instead of qmk/qmk_firmware). This is what Oryx (the graphical configurator) uses, so it's guaranteed to work.
+- Create a folder with a simple name (no spaces!) for your layout inside the qmk_firmware/keyboards/keymaps/ folder.
+- Copy the contents of the \*\_source folder (in the .zip you downloaded from Oryx) into this folder.
+- Make sure you've set up your environment per the [QMK docs](https://docs.qmk.fm/#/newbs_getting_started?id=set-up-your-environment) so compilation would actually work.
+- From your shell, make sure your working directory is qmk*firmware, then enter the command `make :_layout_`, substituting the name of the folder you created for "_layout_".
 
-![Layer 1 Sym + Num](https://github.com/Mvzundert/voyager-layout/assets/6066795/7f297211-86a1-4cb9-9ff4-25518ff3b495)
-
-## Layer 2: Brd + Sys2
-
-![layer 4 Brd + Sys2](https://github.com/Mvzundert/voyager-layout/assets/6066795/952baedc-1e42-4fd3-8271-331fec4f55c4)
+Good luck on your journey! And remember, if you get stuck, you can always get back to your [original layout](https://configure.zsa.io/voyager/layouts/alr9v/WBGjl/0) from Oryx.
--- voyager_marzun-voyager_source/keymap.c
+++ voyager_marzun-voyager_source/keymap.c
@@ -6,13 +6,14 @@ enum custom_keycodes {
   RGB_SLD = ML_SAFE_RANGE,
   ST_MACRO_0,
   ST_MACRO_1,
+  ST_MACRO_2,
 };
 
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
-    KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
+    MT(MOD_LALT, KC_ESCAPE),KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
     MT(MOD_LCTL, KC_DELETE),KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,        
     KC_LEFT_SHIFT,  KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_QUOTE,       
     KC_LEFT_GUI,    KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       OSL(1),         
@@ -21,15 +22,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT_voyager(
     KC_TILD,        KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,                                          KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,         
     KC_GRAVE,       KC_EXLM,        KC_AT,          KC_HASH,        KC_DLR,         KC_PERC,                                        KC_7,           KC_8,           KC_9,           KC_MINUS,       KC_SLASH,       KC_F12,         
-    KC_TRANSPARENT, KC_CIRC,        KC_AMPR,        KC_ASTR,        KC_LPRN,        KC_RPRN,                                        KC_4,           KC_5,           KC_6,           KC_PLUS,        KC_ASTR,        KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_CIRC,        KC_AMPR,        KC_ASTR,        KC_LPRN,        KC_RPRN,                                        KC_4,           KC_5,           KC_6,           KC_PLUS,        KC_ASTR,        QK_LLCK,        
     KC_TRANSPARENT, KC_TRANSPARENT, KC_LBRC,        KC_RBRC,        KC_LCBR,        KC_RCBR,                                        KC_1,           KC_2,           KC_3,           KC_DOT,         KC_EQUAL,       KC_TRANSPARENT, 
                                                     KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_0
   ),
   [2] = LAYOUT_voyager(
-    RGB_TOG,        TOGGLE_LAYER_COLOR,RGB_MODE_FORWARD,RGB_SLD,        RGB_VAD,        RGB_VAI,                                        KC_PAGE_UP,     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, LCTL(LSFT(KC_UP)),LCTL(LSFT(KC_DOWN)),
-    ST_MACRO_0,     LALT(LGUI(KC_E)),KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,KC_AUDIO_MUTE,  KC_TRANSPARENT,                                 KC_PGDN,        KC_HOME,        KC_END,         KC_TRANSPARENT, LCTL(LSFT(KC_LEFT)),LCTL(LSFT(KC_RIGHT)),
-    ST_MACRO_1,     KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,KC_MEDIA_STOP,  KC_MEDIA_PLAY_PAUSE,KC_TRANSPARENT,                                 KC_LEFT,        KC_UP,          KC_DOWN,        KC_RIGHT,       LALT(LGUI(LCTL(KC_LEFT))),LALT(LGUI(LCTL(KC_RIGHT))),
-    KC_TRANSPARENT, KC_LEFT_CTRL,   KC_LEFT_ALT,    KC_LEFT_GUI,    KC_TRANSPARENT, KC_TRANSPARENT,                                 LALT(LGUI(KC_UP)),LGUI(LCTL(KC_LEFT)),LGUI(LCTL(KC_RIGHT)),LALT(LGUI(KC_DOWN)),KC_TRANSPARENT, KC_TRANSPARENT, 
+    ST_MACRO_0,     TOGGLE_LAYER_COLOR,RGB_MODE_FORWARD,RGB_TOG,        RGB_VAD,        RGB_VAI,                                        KC_PAGE_UP,     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, LCTL(LSFT(KC_UP)),LCTL(LSFT(KC_DOWN)),
+    ST_MACRO_1,     LALT(LGUI(KC_E)),KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,KC_AUDIO_MUTE,  KC_TRANSPARENT,                                 KC_PGDN,        KC_HOME,        KC_END,         KC_TRANSPARENT, LCTL(LSFT(KC_LEFT)),LCTL(LSFT(KC_RIGHT)),
+    ST_MACRO_2,     KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,KC_MEDIA_PLAY_PAUSE,KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_LEFT,        KC_UP,          KC_DOWN,        KC_RIGHT,       LALT(LGUI(LCTL(KC_LEFT))),LALT(LGUI(LCTL(KC_RIGHT))),
+    KC_LEFT_SHIFT,  KC_LEFT_CTRL,   KC_LEFT_ALT,    KC_LEFT_GUI,    KC_TRANSPARENT, KC_TRANSPARENT,                                 LALT(LGUI(KC_UP)),LGUI(LCTL(KC_LEFT)),LGUI(LCTL(KC_RIGHT)),LALT(LGUI(KC_DOWN)),KC_TRANSPARENT, KC_TRANSPARENT, 
                                                     KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT
   ),
 };
@@ -87,10 +88,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case ST_MACRO_0:
     if (record->event.pressed) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_A)) SS_DELAY(100) SS_LSFT(SS_TAP(X_T)));
+      SEND_STRING(SS_LCTL(SS_TAP(X_A)) SS_DELAY(100) SS_TAP(X_Q));
     }
     break;
     case ST_MACRO_1:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LCTL(SS_TAP(X_A)) SS_DELAY(100) SS_LSFT(SS_TAP(X_T)));
+    }
+    break;
+    case ST_MACRO_2:
     if (record->event.pressed) {
       SEND_STRING(SS_LCTL(SS_TAP(X_A)) SS_DELAY(100) SS_TAP(X_W));
     }
